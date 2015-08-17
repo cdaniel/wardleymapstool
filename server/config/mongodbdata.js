16cfe2db02b8cd4ce918406f466a05decde0dcbc
Krzysztof Daniel <krzysztof.daniel@gmail.com>
2015-08-17 07:44:33 +0200

    Release openshift integration.

--- server/config/mongodbdata.js
+++ server/config/mongodbdata.js
@@ -21,6 +21,16 @@ function mongodbdata() {
 	
 	this.getConnectionString = function (){
 		var result = "127.0.0.1:27017/wardleymaps";
+		
+		// if OPENSHIFT env variables are present, use the available connection info:
+		if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+		  result = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+		  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+		  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+		  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+		  process.env.OPENSHIFT_APP_NAME;
+		}
+		
 		return result;
 	};
 	
