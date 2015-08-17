2a6bc4f00871e774dcfba6571705a5bbba9341f1
Krzysztof Daniel <krzysztof.daniel@gmail.com>
2015-08-17 08:37:49 +0200

    rely on env variables when configuring google login.

--- server/config/googleauth.js
+++ server/config/googleauth.js
@@ -19,17 +19,17 @@ limitations under the License.*/
 function googleauth() {
 	
 	this.getClientID = function (){
-		var result = '465155999503-93j04e0hsc7h9gl7oora0ectkc69m4vk.apps.googleusercontent.com';
-		if(result === '465155999503-93j04e0hsc7h9gl7oora0ectkc69m4vk.apps.googleusercontent.com'){
-			console.log("please consider creating your own keys!");
+		var result = process.env.WM_GOOGLE_CLIENT_ID || "";
+		if(result === ''){
+			console.log("please consider creating your own google keys!");
 		}
 		return result;
 	};
 	
 	this.getClientSecret = function () {
-		var result = 'MbvdykOgwRvxYCJMzy9OHPtC';
-		if(result === 'MbvdykOgwRvxYCJMzy9OHPtC'){
-			console.log("please consider creating your own keys!");
+	    var result = process.env.WM_GOOGLE_CLIENT_SECRET || "";
+		if(result === ''){
+			console.log("please consider creating your own google keys!");
 		}
 		return result;
 	};
