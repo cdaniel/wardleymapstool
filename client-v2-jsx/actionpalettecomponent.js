d266b48745acf4dca67c17d2bf78497f4e2c8acf
Krzysztof Daniel <krzysztof.daniel@gmail.com>
2015-11-24 22:06:39 +0100

    add toggled state for move button

--- client-v2-jsx/actionpalettecomponent.js
+++ client-v2-jsx/actionpalettecomponent.js
@@ -3,21 +3,36 @@ var React = require('react');
 var Button = require('react-bootstrap').Button;
 var MapComponent = require('./mapcomponent');
 var MapActions = require('./actions/mapactions');
+var jquery = require('jquery');
 
 var outerStyle = {
     position: 'relative'
 };
 
 var ActionPaletteComponent = React.createClass({
+    componentDidMount: function() {
+      this.props.store.addChangeListener(this._onChange);
+    },
+    componentWillUnmount: function() {
+      this.props.store.removeChangeListener(this._onChange);
+    },
+    _onChange: function() {
+      this.setState(this.props.store.getAll());
+    },
+    getInitialState : function() {
+      return {
+        mapMode : null
+      };
+    },
     handleClickMove : function(event){
       MapActions.toggleMode(this.props.toggle);
     },
     render: function() {
       var target_function = this.handleClickMove;
       var store = this.props.store;
-      //TODO: toggledState
+      var active = this.state.mapMode === this.props.toggle;
         return (
-            <Button href="#" style={outerStyle} onClick={target_function}>
+            <Button href="#" style={outerStyle} onClick={target_function} ref="button" active={active}>
                 <div>
                     {this.props.name}
                 </div>
